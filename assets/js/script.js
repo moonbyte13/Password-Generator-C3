e085d79aa54ea35333453815e927fd7076f592cb
Michael Frayne <fraynemichael300@gmail.com>
2023-01-18 17:27:04 -0700

    added randomizer and generate

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,31 +105,85 @@ let numbers = [
   '9'
 ];
 
+// User variables
+let passLength = 0;
+let lowercaseTrue = false;
+let uppercaseTrue = false;
+let symbolsTrue = false;
+let numbersTrue = false;
+
 // Function user data
 function userData() {
+  // Password length prompt
+  passLength = prompt('What length do you want the password to be?');
 
+  if (NaN[passLength]) {
+    alert('INVALID ENTRY, NOT A NUMBER');
+    return;
+  };
+  if (passLength < 8 || passLength > 128) {
+    alert('INVALID, OUT OF PARAMETER');
+    return; 
+  };
+  // Criteria prompts
+  const criteria = [
+    lowercaseTrue = confirm('Do you want lowercase?'),
+    uppercaseTrue = confirm('Do you want uppercase?'),
+    symbolsTrue = confirm('Do you want symbols?'),
+    numbersTrue = confirm('Do you want numbers?')
+  ];
+  
+
+  if (lowercaseTrue == false && uppercaseTrue == false && symbolsTrue == false && numbersTrue == false) {
+    alert('INVALID, NOT A STRONG ENOUGH PASSWORD');
+    return; 
+  };
 }
+// Password array
+const passArray = [''];
 
 // Function generate password
 function generatePassword() {
-
+  while (passLength == !0) {
+    if (criteria.lowercaseTrue == true && passLength >= 0) {
+      passArray.push(lowercaseRan);
+    }
+    if (criteria.uppercaseTrue == true && passLength >= 0) {
+      passArray.push(uppercaseRan);
+    }
+    if (criteria.symbolsTrue == true && passLength >= 0) {
+      passArray.push(symbolRan);
+    }
+    if (criteria.numbersTrue == true && passLength >= 0) {
+      passArray.push(numberRan);
+    }
+  }
 }
 
+// Randomizer variables
+let lowercaseRan = 0;
+let uppercaseRan = 0;
+let symbolRan = 0;
+let numberRan = 0;
+let criteriaRan = 0;
 
 // Randomizer function
 function randomizer() {
-  let userChoice = userData()
-  let choice = Math.floor(Math.random() * (userChoice))
-  
+  lowercaseRan = Math.floor(Math.random() * (lowercase.length));
+  uppercaseRan = Math.floor(Math.random() * (uppercase.length));
+  symbolRan = Math.floor(Math.random() * (symbols.length));
+  numberRan = Math.floor(Math.random() * (numbers.length));
+  criteriaRan = Math.floor()
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  userData()
-  let password = generatePassword();
+  userData();
+  randomizer();
+ /*  let password = generatePassword();
   let passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = password; */
   
 }
 
